501dc015ad428d4e68fe3934421f9d245a3471f4
islandcontroller <13700483+islandcontroller@users.noreply.github.com>
2023-09-21 00:22:42 +0200

    Fixed header include errors; Moved assert info to global debug variables

--- src/stm32f1xx_ll_assert.c
+++ src/stm32f1xx_ll_assert.c
@@ -6,13 +6,22 @@
  * Assert function
  *
  * @date  21.08.2023
+ * @date  21.09.2023  Moved assert info to global debug variables; Removed un-
+ *                    used header include
  ******************************************************************************/
 
 /*- Header files -------------------------------------------------------------*/
-#include "stm32f1xx_hal_def.h"
 #include "stm32f1xx_ll_assert.h"
 
 #ifdef USE_FULL_ASSERT
+/*- Global Variables ---------------------------------------------------------*/
+/*! Assertion file name (debugger access)                                     */
+volatile const char* LL_Assert_FileName = NULL;
+
+/*! Assertion line number (debugger access)                                   */
+volatile uint32_t LL_Assert_LineNumber = 0u;
+
+
 /*!*****************************************************************************
  * @brief
  * Assertion failure
@@ -20,14 +29,10 @@
  * @param[in] pszFile     Source file name
  * @param[in] ulLine      Source file line number
  * @date  21.08.2023
+ * @date  21.09.2023  Moved FileName, LineNumber to global variables for Debug
  ******************************************************************************/
 __weak void LL_Assert_Failed(const char* pszFile, uint32_t ulLine)
 {
-  /* Function template for debug only, override implemented
-   * in application source.                               */
-  static volatile const char* LL_Assert_FileName = NULL;
-  static volatile uint32_t LL_Assert_LineNumber = 0u;
-
   /* Ensure file name and line number are saved           */
   LL_Assert_FileName = pszFile;
   LL_Assert_LineNumber = ulLine;
